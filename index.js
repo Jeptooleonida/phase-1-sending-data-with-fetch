9c9f7affd0bb3454f243d9507d275e7f80acd1b3
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-06-16 02:24:01 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email){
+    const user = {name:name, email: email}
+
+    return fetch ("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "content-Type": "application/json",
+            Accept : "application/json",
+        },
+        body: JSON.stringify (user)
+    })
+    .then (response => response.json())
+    .then (result =>{
+        document.body.innerHTML = result["id"]
+    })
+    .catch(errmsg =>{
+        document.body.innerHTML = errmsg.message
+        alert("name or email is incorect");
+    })
+}
